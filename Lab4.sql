79f2372706b7922f235951e1136578697a85c858
fitzsibc <149700583+fitzsibc@users.noreply.github.com>
2024-09-23 16:01:56 -0400

    Update Lab4.sql

--- Lab4.sql
+++ Lab4.sql
@@ -1,7 +1,7 @@
---4.1 No, you cannot delete VA because it is referred to the other tables. It's on the name table and the population table. This is called data consistency.
+ --4.1 No, you cannot delete VA because it is referred to the other tables. It's on the name table and the population table. This is called data consistency.
 --4.2 No, because it is not a valid fips.   
 --4.3 They are perfect when you give Chat Gpt the right prompt. 
---4.4 
+--4.4 ER Diagram uploaded to ChatGPT.
 --4.5 It recognizes it but not to the fullest extent.
 --4.6 The State with the highest income is Washington D.C. The SQL code worked perfectly. 
 --4.7 WITH population_data AS (
